ae709db65368144afb010818c42432cc4bf65b1b
soa-a319-e-009 <soa-a319-e-009@soa-a313-e-009.UMD.LOCAL.EDU>
2019-11-15 20:53:01 -0500

    ordenamiento binario estructuara

---
+++ src/javaapplication19/JavaApplication19.java
@@ -0,0 +1,44 @@
+
+package javaapplication19;
+
+public class JavaApplication19 {
+
+    
+    void sort(int arr[])
+    {
+        int n= arr.length;
+        for(int i=0;i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+         }
+    }
+    
+    void printArray(int arr[]){
+        int n = arr.length;
+        //imprime las posiciones en orden
+        for (int i=0; i<n; i++)
+            System.out.println(arr[i]+" ");
+    }
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         //crear objeto
+        JavaApplication19 p = new JavaApplication19(); 
+        //hago un arreglo
+        int arr[] = {8,879,456,213,87,465,55,213,21,213};
+        //instanciar metodo sort
+        p.sort(arr);
+        //imprimo el arreglo
+        System.out.println(arr);
+        //instanciar metodo imprimir
+        p.printArray(arr);
+    }
+    
+}
